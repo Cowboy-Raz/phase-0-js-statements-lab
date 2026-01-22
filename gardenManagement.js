6e2804c04c90af31959f361fdaeb6fd66a222a54
Raz <christopher.d.rasberry@gmail.com>
2026-01-22 17:47:34 -0500

    Updated Statements Lab gardenManagement

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,22 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+    if (temperature > 90) {
+    console.log("Watering on");
+} else {
+    console.log("Watering Off");
+}
+
+    if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+while (soilMoisture < 40) {
+    soilMoisture = soilMoisture + 5;
+    if (soilMoisture === 40) {
+        console.log(40);
+    }
+}
